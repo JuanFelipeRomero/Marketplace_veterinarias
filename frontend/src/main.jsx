1416a5d795f2aa590061875ba12bd6f2e186c949
juan <juanfeliperomerocastro09@gmail.com>
2024-11-13 22:47:42 -0500

    React router configured :D

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,24 @@
-import { Provider } from '@/components/ui/provider'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { Provider } from '@/components/ui/provider';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+
+//Importacion de paginas / componentes
+import HomePage from './pages/HomePage.jsx';
+
+const router = createBrowserRouter([
+  //Home page
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
-)
+);
